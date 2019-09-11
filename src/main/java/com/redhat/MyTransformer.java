5b9f17a2183eefe667281d91bf7cbd86ef3116aa
ws <wohshon@gmail.com>
2019-09-11 08:27:28 +0800

    changed content type

--- src/main/java/com/redhat/MyTransformer.java
+++ src/main/java/com/redhat/MyTransformer.java
@@ -16,7 +16,10 @@
  */
 package com.redhat;
 
+import org.apache.camel.Exchange;
 import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
 
 /**
  * A sample transform
@@ -24,15 +27,13 @@ import org.springframework.stereotype.Component;
 @Component(value = "myTransformer")
 public class MyTransformer {
 
-    public String transform() {
-        // let's return a random string
-        StringBuffer buffer = new StringBuffer();
-        for (int i = 0; i < 3; i++) {
-            int number = (int) (Math.round(Math.random() * 1000) % 10);
-            char letter = (char) ('0' + number);
-            buffer.append(letter);
-        }
-        return buffer.toString();
+    public String transform(Exchange e) {
+    	String j=new String((byte[])e.getIn().getBody());
+    	System.out.println("*****"+j);
+    	Gson gson = new Gson();
+    	e.getIn().setHeader(Exchange.CONTENT_TYPE, "text/plain");
+
+    	return new String(j);
     }
 
 }
